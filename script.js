7845dfa97b45361c4f16aa29fd1e34049b178d0f
Godwin <ohenebayb@gmail.com>
2024-02-07 16:05:22 +0000

    justify event added

--- script.js
+++ script.js
@@ -9,16 +9,33 @@
 //   });
 // });
 
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 2, 2024 00:00:00").getTime();
 
-window.onscroll = funtion();{myFunction()};
+// Update the countdown every 1 second
+var x = setInterval(function() {
 
-var header = docuemt.getElementById('myHeader');
-var sticky = header.offsetTop;
+  // Get the current date and time
+  var now = new Date().getTime();
 
-function myFunction() {
-  if(window.pageYOffset > sticky){
-    header.classList.add('sticky');
-  } else {
-    header.classList.remove(sticky);
+  // Calculate the distance between now and the countdown date
+  var distance = countDownDate - now;
+
+  // Calculate days, hours, minutes, and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the corresponding elements
+  document.getElementById("days").innerHTML = days.toString().padStart(2, '0');
+  document.getElementById("hours").innerHTML = hours.toString().padStart(2, '0');
+  document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, '0');
+  document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, '0');
+
+  // If the countdown is finished, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
   }
-}
+}, 1000);
